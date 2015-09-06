705f6cc66252fc7aae641ce33b3e791b26f7c6fd
Vasily <vasily.petruhin@gmail.com>
2015-09-06 18:49:14 +0300

    Initial commit of ElevateTool

---
+++ ElevateTool/Elevate/Elevate.cpp
@@ -0,0 +1,37 @@
+#include "stdafx.h"
+
+
+void printHelp()
+{
+	//..
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	if (argc == 1)
+	{
+		printHelp();
+		return 0;
+	}
+
+	if (IsWindowsVistaOrGreater())
+	{
+		SHELLEXECUTEINFOW sei;
+		ZeroMemory(&sei, sizeof(SHELLEXECUTEINFOW));
+		sei.cbSize = sizeof(SHELLEXECUTEINFOW);
+		sei.fMask = SEE_MASK_NOCLOSEPROCESS;
+		if (IsWindowsVistaOrGreater())
+		{
+			sei.lpVerb = L"runas";
+		}
+		sei.lpFile = argv[1];
+		sei.lpParameters = argc > 2 ? argv[2] : nullptr;
+		sei.nShow = SW_SHOWNORMAL;
+		if (TRUE == ShellExecuteExW(&sei))
+		{
+			CloseHandle(sei.hProcess);
+		}
+	}
+
+	return 0;
+}
---
+++ ElevateTool/Elevate/stdafx.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN
+#define STRICT
+#include <windows.h>
+#include <Shellapi.h>
+#include <VersionHelpers.h>
+
+#include <set>
+//#include <stdio.h>
+#include <tchar.h>
