4d0307878b5fad52add98096022f49c80d5140f7
Vasily <vasily.petruhin@gmail.com>
2015-09-07 11:04:10 +0300

    Implemented command line support

--- ElevateTool/Elevate/Elevate.cpp
+++ ElevateTool/Elevate/Elevate.cpp
@@ -23,9 +23,32 @@ elevateArgs* parseCommandLine(int argc, wchar_t** argv)
 		pArgs->pParameters = argv[1];
 	else
 	{
-		//int len =
+		int len = 0;
+		for (int i = 1; i < argc; i++)
+		{
+			len += lstrlenW(argv[i]);
+		}
+		int spaceCount = argc - 2;
+		int remainingSize = len + 1 + spaceCount;
+		wchar_t* pParameters = new wchar_t[remainingSize];
+		wchar_t* pTemp = pParameters;
+		for (int i = 1, pos = 0; i < argc; i++)
+		{
+			int curLen = lstrlenW(argv[i]);
+			StringCchCopyW(pTemp, remainingSize, argv[i]);
+			remainingSize -= curLen;
+			pTemp += curLen;
+
+			if (spaceCount > 0)
+			{
+				StringCchCopyW(pTemp, remainingSize, L" ");
+				remainingSize -= 1;
+				pTemp += 1;
+				spaceCount--;
+			}
+		}
+		pArgs->pParameters = pParameters;
 	}
-	//pArgs->pParameters = argc > 1 ? argv[1] : nullptr;
 	return pArgs;
 }
 
--- ElevateTool/Elevate/stdafx.h
+++ ElevateTool/Elevate/stdafx.h
@@ -4,12 +4,8 @@
 
 #define WIN32_LEAN_AND_MEAN
 #define STRICT
-//#define _UNICODE
 
 #include <windows.h>
-#include <Shellapi.h>
+#include <shellapi.h>
+#include <strsafe.h>
 #include <VersionHelpers.h>
-
-//#include <set>
-//#include <stdio.h>
-//#include <tchar.h>
